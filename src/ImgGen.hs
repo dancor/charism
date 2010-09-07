cdd3b925d7716b067d7879a4f7d5cd1fe574bf99
Daniel Corson <danl@alum.mit.edu>
2010-09-07 04:05:54 -0400

    imggen for _ and help and for new wumpus version

--- src/ImgGen.hs
+++ src/ImgGen.hs
@@ -1,26 +1,42 @@
 import Data.Char
 import Wumpus.Core
 
-black :: PSRgb
-black = RGB3 0 0 0
+black :: RGBi
+black = RGBi 0 0 0
 
-white :: PSRgb
-white = RGB3 1 1 1
+white :: RGBi
+white = RGBi 255 255 255
 
-mkFilledSquare :: (PSColour c, Fill c) => c -> Double -> DPicture
-mkFilledSquare col n = frame $ fill col $ vertexPath
-  [P2 0 0, P2 n 0, P2 n n, P2 0 n]
+green :: RGBi
+green = RGBi 0 127 0
+
+mkFilledSquare :: RGBi -> Double -> DPicture
+mkFilledSquare col n = mkFilledRect col n n
+
+mkFilledRect :: RGBi -> Double -> Double -> DPicture
+mkFilledRect col w h = frame [
+  fill col $ vertexPath [P2 0 0, P2 w 0, P2 w h, P2 0 h]
+  ]
 
 main :: IO ()
-main = mapM_ makeLtr ['a'..'z']
+main = do
+  mapM_ makeLtr ['A'..'Z']
+  writeSVG_latin1 "@.svg" $ multi [
+    translate 10 10 $ mkFilledRect white 300 30,
+    mkFilledSquare green 320
+    ]
+  writeSVG_latin1 "help.svg" $ frame [
+    textlabel black (FontAttr 20 $ FontFace "monospace" "monospace" SVG_BOLD)
+      "help" (P2 0 (0 :: Double))
+    ]
 
-makeLtr c = do
-  writeSVG_latin1 (c:".svg") pic where
-  pic :: Picture Double
-  pic = foldr1 picOver $ reverse [
-    mkFilledSquare black 320,
-    translate 10 10 $ mkFilledSquare white 300,
-    translate 60 60 . frame $ 
-      textlabel (FontAttr 330 $ FontFace "monospace" "monospace" SVG_BOLD)
-        [toUpper c] (P2 0 0)]
+makeLtr :: Char -> IO ()
+makeLtr c = writeSVG_latin1 (c:".svg") $ multi [
+  translate 60 (if c == 'Q' then 60 else 40) $ frame [
+    textlabel black (FontAttr 330 $ FontFace "monospace" "monospace" SVG_BOLD)
+      [c] (P2 0 0)
+    ],
+  translate 10 10 $ mkFilledSquare white 300,
+  mkFilledSquare black 320
+  ]
 
